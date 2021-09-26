195f6cb2d2273b504563b258477b8206a2f9726b
thahair <m.d.thahair@gmail.com>
2021-09-26 16:53:34 +0530

    filter-component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 
 class App extends React.Component {
 
@@ -11,9 +12,47 @@ class App extends React.Component {
             products: data.products,
             size: "",
             sort: ""
+        };
+    }
+    sortProducts = (event) => {
+        const sort = event.target.value;
+        this.setState((state) => ({
+            sort: sort,
+            products: this.state.products
+                .slice()
+                .sort((a, b) =>
+                    sort === "lowest"
+                        ? a.price > b.price
+                            ? 1
+                            : -1
+                        : sort === "highest"
+                            ? a.price < b.price
+                                ? 1
+                                : -1
+                            : a._id > b._id
+                                ? 1
+                                : -1),
+        }));
+    }
+    filterProducts = (event) => {
+        console.log(event.target.value);
+        if (event.target.value === "") {
+            this.setState({
+                size: event.target.value,
+                products: data.products,
+                sort: ""
+            })
+        }
+        else {
+            this.setState({
+                size: event.target.value,
+                products: data.products
+                    .filter(product => product.availableSizes
+                        .indexOf(event.target.value) >= 0),
+                sort: ""
+            })
         }
     }
-
     render() {
         return (
             <div className="grid-container">
@@ -23,6 +62,13 @@ class App extends React.Component {
                 <main>
                     <div className="content">
                         <div className="main">
+                            <Filter
+                                count={this.state.products.length}
+                                size={this.state.size}
+                                sort={this.state.sort}
+                                filterProducts={this.filterProducts}
+                                sortProducts={this.sortProducts}
+                            />
                             <Products products={this.state.products} />
                         </div>
                         <div className="sidebar">
